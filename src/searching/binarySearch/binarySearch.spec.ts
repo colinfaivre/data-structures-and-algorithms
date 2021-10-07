932cd1e03e37456bc126ed62b59ca996a6339d1a
Colin Faivre <colin.faivre@gmail.com>
2021-10-07 15:55:11 +0200

    Add binarySearch test and implementation

--- src/searching/binarySearch/binarySearch.spec.ts
+++ src/searching/binarySearch/binarySearch.spec.ts
@@ -1,5 +1,5 @@
 import { binarySearch } from "./binarySearch";
 
 test('binarySearch()', () => {
-    expect(true).toEqual(true);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9)).toEqual(8);
 });
--- src/searching/binarySearch/binarySearch.ts
+++ src/searching/binarySearch/binarySearch.ts
@@ -1,5 +1,27 @@
-export function binarySearch(arrayInWhichSearch: number[], valueToSearch: number): [boolean, number?] {
-    
-    
-    return [true];
+import { insertionSort } from "../../sorting/insertionSort/insertionSort";"../../sorting/insertionSort"
+
+export function binarySearch(
+    arrayInWhichSearch: number[],
+    valueToSearch: number
+): number {
+    // Sort the array first
+    const sortedArray = insertionSort(arrayInWhichSearch);
+    // Initialize left and right bounds
+    let leftIndex = 0;
+    let rightIndex = sortedArray.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const middleIndex = Math.floor((rightIndex + leftIndex) / 2);
+        const middleValue = sortedArray[middleIndex];
+
+        if (middleValue < valueToSearch) {
+            leftIndex = middleIndex + 1;
+        } else if (middleValue > valueToSearch) {
+            rightIndex = middleIndex - 1;
+        } else {
+            return middleIndex
+        }
+    }
+
+    return -1;
 }
